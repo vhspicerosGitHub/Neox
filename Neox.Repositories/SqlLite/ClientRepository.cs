24afbef8a9486f1c74e9187180bf1d589df509f8
Victor Hugo Saavedra <vhspiceros+GitHub@gmail.com>
2023-12-08 22:15:11 -0300

    Se agrega connectionString al app.setting

--- Neox.Repositories/SqlLite/ClientRepository.cs
+++ Neox.Repositories/SqlLite/ClientRepository.cs
@@ -2,49 +2,58 @@
 using Microsoft.Extensions.Logging;
 using Neox.Model;
 using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Runtime.CompilerServices;
 
 namespace Neox.Repositories.SqlLite
 {
     public class ClienteRepository : IClientRepository
     {
         private readonly ILogger<ClienteRepository> _logger;
-        private readonly string _connectionString = "Data Source=../Database/Database.sqlite";
+        private readonly IConfiguration _configuration;
 
-        public ClienteRepository(ILogger<ClienteRepository> logger)
+        public ClienteRepository(ILogger<ClienteRepository> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
+        private SqliteConnection GetConnection()
+        {
+            var connString = _configuration.GetConnectionString("DefaultConnection");
+            return new SqliteConnection(connString);
+
+        }
         public async Task<int> Create(Client client)
         {
-            return await new SqliteConnection(_connectionString).ExecuteScalarAsync<int>(Queries.Create,
+            return await GetConnection().ExecuteScalarAsync<int>(Queries.Create,
                 new { name = client.Name, email = client.Email });
         }
 
         public async Task<IEnumerable<Client>> GetAll()
         {
-            return await new SqliteConnection(_connectionString).QueryAsync<Client>(Queries.GetAll);
+            return await GetConnection().QueryAsync<Client>(Queries.GetAll);
         }
 
         public async Task<Client?> GetByEmail(string email)
         {
-            return await new SqliteConnection(_connectionString).QueryFirstOrDefaultAsync<Client>(Queries.GetByEmail,
+            return await GetConnection().QueryFirstOrDefaultAsync<Client>(Queries.GetByEmail,
                 new { email });
         }
 
         public async Task<Client?> GetById(int id)
         {
-            return await new SqliteConnection(_connectionString).QueryFirstOrDefaultAsync<Client>(Queries.GetById, new { id });
+            return await GetConnection().QueryFirstOrDefaultAsync<Client>(Queries.GetById, new { id });
         }
 
         public async Task Delete(Client client)
         {
-            _ = await new SqliteConnection(_connectionString).ExecuteAsync(Queries.Delete, new { id = client.Id });
+            _ = await GetConnection().ExecuteAsync(Queries.Delete, new { id = client.Id });
         }
 
         public async Task Update(Client client)
         {
-            _ = await new SqliteConnection(_connectionString).ExecuteAsync(Queries.Update,
+            _ = await GetConnection().ExecuteAsync(Queries.Update,
                 new { id = client.Id, name = client.Name, email = client.Email });
         }
     }
